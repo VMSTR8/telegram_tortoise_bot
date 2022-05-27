a51b3613a0212855a808e53cd5ef4c99a75c0fff
Maxim Vinokurov <vmstr8@yandex.ru>
2022-05-27 23:33:04 +0300

    Описаны новые модели

---
+++ database/user/models.py
@@ -0,0 +1,37 @@
+from tortoise.models import Model
+from tortoise import fields
+
+
+class Team(Model):
+    id = fields.IntField(pk=True)
+    title = fields.CharField(max_length=255, unique=True, null=False)
+
+    def __str__(self):
+        return self.title
+
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    telegram_id = fields.IntField(unique=True, null=False)
+    callsign = fields.CharField(max_length=255, unique=True, null=True)
+    is_member = fields.BooleanField(default=False)
+    is_admin = fields.BooleanField(default=False)
+    team: fields.ForeignKeyRelation[Team] = fields.ForeignKeyField(
+        'models.Team', related_name='users', null=True, on_delete=fields.RESTRICT
+    )
+
+    def __str__(self):
+        return self.telegram_id
+
+
+class Location(Model):
+    id = fields.IntField(pk=True)
+    point = fields.CharField(max_length=255, unique=True, null=False)
+    latitude = fields.FloatField(default=00.000000, null=False)
+    longitude = fields.FloatField(default=00.000000, null=False)
+    team: fields.OneToOneRelation[Team] = fields.OneToOneField(
+        'models.Team', related_name='locations', null=True, on_delete=fields.RESTRICT
+    )
+
+    def __str__(self):
+        return self.point
