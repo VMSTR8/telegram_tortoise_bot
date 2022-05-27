dda6ff3eb5a54584613f1a1ab2508442b2f08544
Maxim Vinokurov <vmstr8@yandex.ru>
2022-05-27 23:32:48 +0300

    Файл переименован

--- jobs/start.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-# TODO изменить приветственное сообщение
-
-
-async def start(update: Update, context: CallbackContext) -> None:
-    """Say 'Hello!' to user and inform about abilities of the bot"""
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Тут приветственное сообщение')
