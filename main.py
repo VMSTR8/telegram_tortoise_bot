19792739c80825ef79b0e2a1c0c845ce0280b0f1
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-03 04:04:31 +0300

    Приведены в порядок импорты. Убрана функция, которая выводила пользователю информацию о том, что сообщение не распознано. Пока что функция не требуется. Добавлен обработчик присоединения к стороне.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import logging
 
 from telegram.ext import CommandHandler, MessageHandler, \
-    ApplicationBuilder, filters, CallbackQueryHandler
+    ApplicationBuilder, filters, CallbackQueryHandler, ConversationHandler
 
 from tortoise import run_async
 
 from database.init import init
 
-from handlers.menu import CREATE_OR_UPDATE_CALLSIGN, start, callsign, \
-    commit_callsign, stop_calsign
-from handlers.admin_command import *
+from handlers.menu import CREATE_OR_UPDATE_CALLSIGN, CHOOSING_TEAM_ACTION, \
+    start, callsign, commit_callsign, stop_callsign_handler, team, \
+    choose_the_team, stop_team_handler
+
+from handlers.admin_command import admin, ADD_TEAM, EDIT_TEAM, DELETE_TEAM, \
+    END, SELECTING_ACTION, ENTERING_TEAM, STOPPING, adding_team, end, \
+    commit_team, stop_admin_handler
 
 from settings.settings import BOT_TOKEN
 
@@ -17,12 +21,6 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
-
-async def unrecognized_message(update: Update,
-                               context: CallbackContext.DEFAULT_TYPE) -> None:
-    text = 'Нераспознанная команда.'
-    await update.message.reply_text(text=text)
 
 
 if __name__ == '__main__':
@@ -40,10 +38,24 @@ if __name__ == '__main__':
         entry_points=[CommandHandler('callsign', callsign)],
         states={
             CREATE_OR_UPDATE_CALLSIGN: [
-                MessageHandler(filters.TEXT & (~ filters.COMMAND), commit_callsign)
+                MessageHandler(
+                    filters.TEXT & (~ filters.COMMAND), commit_callsign
+                )
             ]
         },
-        fallbacks=[MessageHandler(filters.COMMAND, stop_calsign)],
+        fallbacks=[MessageHandler(filters.COMMAND, stop_callsign_handler)],
+    )
+
+    team_handler = ConversationHandler(
+        allow_reentry=True,
+        entry_points=[CommandHandler('team', team)],
+        states={
+            CHOOSING_TEAM_ACTION: [CallbackQueryHandler(
+                callback=choose_the_team,
+                pattern="^" + 'TEAM_COLOR_' + ".*$"),
+            ],
+        },
+        fallbacks=[MessageHandler(filters.COMMAND, stop_team_handler)],
     )
 
     selection_handlers = [
@@ -63,12 +75,13 @@ if __name__ == '__main__':
             )],
             STOPPING: [CommandHandler('admin', admin)]
         },
-        fallbacks=[MessageHandler(filters.COMMAND, stop)]
+        fallbacks=[MessageHandler(filters.COMMAND, stop_admin_handler)]
     )
 
     application.add_handler(start_handler, 0)
     application.add_handler(reg_handler, 1)
-    application.add_handler(admin_handler, 2)
+    application.add_handler(team_handler, 2)
+    application.add_handler(admin_handler, 3)
 
     # Run the app
     application.run_polling()
