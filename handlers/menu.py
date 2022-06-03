3ccd3af2498c70f3c9026b48e960717e540c9749
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-03 04:05:31 +0300

    Приведены в порядок импорты. Написан функционал выбора стороны пользователем. Так же устранены мелкие изменения, которые не требуют подробного описания.

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,16 +1,18 @@
 import re
 import string
 
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext, ConversationHandler
 
-from tortoise.exceptions import IntegrityError, ValidationError
+from tortoise.exceptions import IntegrityError, ValidationError, DoesNotExist
 
 from settings.settings import CREATORS_ID
 
 from database.user.models import User
 
-CREATE_OR_UPDATE_CALLSIGN = 0
+from database.db_functions import get_teams, update_players_team
+
+CREATE_OR_UPDATE_CALLSIGN, CHOOSING_TEAM_ACTION = map(chr, range(2))
 
 END = ConversationHandler.END
 
@@ -35,7 +37,6 @@ async def start(update: Update,
 async def callsign(update: Update,
                    context: CallbackContext.DEFAULT_TYPE) -> \
         CREATE_OR_UPDATE_CALLSIGN:
-
     user = update.message.from_user.id
 
     if user == int(CREATORS_ID):
@@ -47,8 +48,8 @@ async def callsign(update: Update,
 
     await update.message.reply_text(
         'Введи свой позывной в текстовом поле и нажми отправить.\n\n'
-        'Позывной должен быть уникальным, поэтому, если он уже занят, '
-        'то я тебя уведмлю об этом. Так же в позывном нельзя '
+        'Позывной должен быть уникальным и на латинице, поэтому, если '
+        'он уже занят, то я тебя уведмлю об этом. Так же в позывном нельзя '
         'использовать спец. символы, я их просто удалю.\n\n'
         'Для отмены регистрации позывного напиши /cancel в чат.'
     )
@@ -58,7 +59,6 @@ async def callsign(update: Update,
 
 async def commit_callsign(update: Update,
                           context: CallbackContext.DEFAULT_TYPE) -> END:
-
     user = update.message.from_user.id
     text = update.message.text
     text = re.sub(r'[.\W.\d]', '', text)
@@ -74,7 +74,8 @@ async def commit_callsign(update: Update,
 
     except IntegrityError:
         await update.message.reply_text(
-            'Ошибка, такой позывной уже занят. Попробуй еще раз.\n\n'
+            'Ошибка, такой позывной уже занят или написан кириллицей, а надо латиницей. '
+            'Попробуй еще раз.\n\n'
             'Напоминаю, если хочешь отменить регистрацию позывного, '
             'напиши /cancel в чат.'
         )
@@ -86,11 +87,72 @@ async def commit_callsign(update: Update,
         )
 
 
-async def stop_calsign(update: Update,
-                       context: CallbackContext.DEFAULT_TYPE) -> END:
-
+async def stop_callsign_handler(update: Update,
+                                context: CallbackContext.DEFAULT_TYPE) -> \
+        END:
     await update.message.reply_text(
         'Обновление позывного отменено.'
     )
+
+    return END
+
+
+async def team(update: Update,
+               context: CallbackContext.DEFAULT_TYPE) -> \
+        CHOOSING_TEAM_ACTION:
+    buttons = []
+    teams = await get_teams()
+
+    for team_title in teams:
+        buttons.append(
+            [
+                InlineKeyboardButton(team_title.capitalize(),
+                                     callback_data=str(
+                                         f'TEAM_COLOR_{team_title.upper()}')
+                                     )
+            ]
+        )
+
+    keyboard = InlineKeyboardMarkup(buttons)
+
+    await update.message.reply_text(
+        text='Выбери сторону из предложенных ниже:',
+        reply_markup=keyboard
+    )
+
+    return CHOOSING_TEAM_ACTION
+
+
+async def choose_the_team(update: Update,
+                          context: CallbackContext.DEFAULT_TYPE) -> \
+        END:
+    await update.callback_query.answer()
+
+    callback_data = re.sub(r'^TEAM_COLOR_', '', update.callback_query.data)
+    callback_data = callback_data.lower()
+
+    try:
+        await update_players_team(
+            int(update.callback_query.from_user.id), str(callback_data)
+        )
+
+        text = f'Выбрана сторона: {callback_data.capitalize()}'
+        await update.callback_query.edit_message_text(text=text)
+
+    except DoesNotExist:
+        text = 'Что-то пошло не так. Скорее всего ты не ' \
+               'зарегистрирован(а). Можешь сделать это при ' \
+               'помощи команды /callsign'
+        await update.callback_query.edit_message_text(text=text)
+
+    return END
+
+
+async def stop_team_handler(update: Update,
+                            context: CallbackContext.DEFAULT_TYPE) -> \
+        END:
+    text = 'Выбор стороны прекращен. Если нужно выбрать сторону, ' \
+           'повторно введи /team в чат.'
+    await update.message.reply_text(text=text)
 
     return END
