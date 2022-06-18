0541a0e30a253696386db4527b437d1d2d4a99bd
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-18 17:29:25 +0300

    Добавлена функция success_activation и ее синхронный вариант sync_success_activation, который требуется для запуска таймера. Реализован запуск таймера в отдельном потоке, по истечению которого произойдет активация точки. Ну, импорты оптимизировал. Заходишь ко мне посмотреть код, а тут "красивое".

--- handlers/location.py
+++ handlers/location.py
@@ -1,16 +1,50 @@
+import asyncio
+import threading
+
 from telegram import Update
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, ApplicationBuilder
 
 from geopy import distance
 
 from tortoise.exceptions import DoesNotExist
 
-from database.db_functions import get_points, \
-    update_points_team_id, get_users_team_id
+from database.db_functions import (
+    get_points,
+    update_points_team_id,
+    get_users_team_id,
+    get_point_time,
+    update_points_in_game_status,
+    get_team_title_by_team_id,
+    get_users_telegram_id
+)
+
+from settings.settings import BOT_TOKEN
+
+
+async def success_activation(point_id: int,
+                             point: str,
+                             team: str) -> \
+        None:
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    await update_points_in_game_status(
+        point_id=point_id,
+        status=False
+    )
+
+    text = f'{point} была подорвана стороной {team}.'
+    for user_telegram_id in await get_users_telegram_id():
+
+        await app.bot.send_message(chat_id=user_telegram_id, text=text)
+
+
+def sync_success_activation(*args):
+    asyncio.run(success_activation(*args))
 
 
 async def point_activation(update: Update,
-                           context: CallbackContext) -> None:
+                           context: CallbackContext.DEFAULT_TYPE) -> \
+        None:
     try:
         message = None
 
@@ -20,32 +54,63 @@ async def point_activation(update: Update,
             message = update.message
 
         points = await get_points()
-        user_point = [{'lat': message.location.latitude, 'lng': message.location.longitude}]
+        user_point = [
+            {
+                'lat': message.location.latitude,
+                'lng': message.location.longitude
+            }
+        ]
         radius = 10
 
         user_point_tuple = tuple(user_point[0].values())
 
         team_id = await get_users_team_id(message.from_user.id)
+        team_name = await get_team_title_by_team_id(team_id)
 
         complete_status = False
 
         for point in points:
-            # point = {'id': 3, 'point': 'home_point', 'latitude': 55.604003, 'longitude': 37.663903, 'team_id': None}
             point_tuple = (point['latitude'], point['longitude'])
             dis = distance.distance(point_tuple, user_point_tuple).m
 
             if int(dis) <= radius and point['team_id'] != team_id:
+
+                timer = threading.Timer(
+                    interval=await get_point_time(point_id=point['id']),
+                    function=sync_success_activation,
+                    args=[
+                        point['id'],
+                        point['point'].capitalize(),
+                        team_name
+                    ]
+                )
+
+                timer.name = point['point']
+
+                for thread in threading.enumerate():
+                    if thread.name == point['point']:
+                        thread.cancel()
+
+                timer.start()
+
                 complete_status = True
+
+                activation_text = f"{message.from_user.name}, " \
+                                  f"{point['point'].capitalize()} активирована"
+
                 await update_points_team_id(point_id=point['id'],
                                             team_id=team_id)
 
-                await message.reply_text(text=f"{message.from_user.name}, "
-                                              f"{point['point'].capitalize()} активирована")
+                await message.reply_text(text=activation_text)
 
             elif int(dis) <= radius and point['team_id'] == team_id:
                 complete_status = True
+
+                already_active_text = 'Точка уже активирована ' \
+                                      'твоей игровой стороной.'
+
                 await message.reply_text(
-                    text='Точка уже активирована твоей игровой стороной'
+                    text=already_active_text
                 )
 
             elif complete_status:
@@ -55,12 +120,13 @@ async def point_activation(update: Update,
                 continue
 
         if complete_status is False:
-            await message.reply_text('Ни одна из точек не была достигнута.')
+            not_reached_text = 'Ни одна из точек не была достигнута.'
+            await message.reply_text(text=not_reached_text)
 
     except DoesNotExist:
-        await update.message.reply_text(
-            text='Чтобы активировать точку, тебе нужно примкнуть '
-                 'к игровой стороне.\n\n'
-                 'Для этого необходимо зарегистрироваться при помощи команды:\n/callsign\n\n'
-                 'Затем выбрать сторону при помощи команды:\n/team'
-        )
+        text = 'Чтобы активировать точку, тебе нужно примкнуть ' \
+               'к игровой стороне.\n\n' \
+               'Для этого необходимо зарегистрироваться при ' \
+               'помощи команды:\n/callsign\n\n' \
+               'Затем выбрать сторону при помощи команды:\n/team'
+        await update.message.reply_text(text=text)
