b8b97a9142b24e389d981af2a316cccb5a7509bb
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-18 17:31:17 +0300

    Импорты приведены к состоянию "показываете? красивое". Ну и код чутка подправлен, чтобы flake8 не ругался.

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import logging
 
-from telegram.ext import CommandHandler, MessageHandler, \
-    ApplicationBuilder, filters, CallbackQueryHandler, ConversationHandler
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    ApplicationBuilder,
+    filters,
+    CallbackQueryHandler,
+    ConversationHandler
+)
 
 from tortoise import run_async
 
 from database.init import init
 
-from handlers.menu import CREATE_OR_UPDATE_CALLSIGN, CHOOSING_TEAM_ACTION, \
-    start, callsign, commit_callsign, stop_callsign_handler, team, \
-    choose_the_team, stop_team_handler
+from handlers.menu import (
+    CREATE_OR_UPDATE_CALLSIGN,
+    CHOOSING_TEAM_ACTION,
+    start,
+    callsign,
+    commit_callsign,
+    stop_callsign_handler,
+    team,
+    choose_the_team,
+    stop_team_handler
+)
 
-from handlers.admin_command import admin, ADD_TEAM, EDIT_TEAM, DELETE_TEAM, \
-    END, SELECTING_ACTION, ENTERING_TEAM, STOPPING, adding_team, end, \
-    commit_team, stop_admin_handler
+from handlers.admin_command import (
+    admin,
+    ADD_TEAM,
+    EDIT_TEAM,
+    DELETE_TEAM,
+    END,
+    SELECTING_ACTION,
+    ENTERING_TEAM,
+    STOPPING,
+    adding_team,
+    end,
+    commit_team,
+    stop_admin_handler
+)
 
 from handlers.location import point_activation
 
@@ -61,7 +86,6 @@ if __name__ == '__main__':
     )
 
     selection_handlers = [
-        # editing_team,
         CallbackQueryHandler(adding_team, pattern="^" + str(ADD_TEAM) + "$"),
         CallbackQueryHandler(adding_team, pattern="^" + str(EDIT_TEAM) + "$"),  # adding_team временная
         CallbackQueryHandler(adding_team, pattern="^" + str(DELETE_TEAM) + "$"),  # adding_team временная
@@ -80,7 +104,9 @@ if __name__ == '__main__':
         fallbacks=[MessageHandler(filters.COMMAND, stop_admin_handler)]
     )
 
-    point_activation_handler = MessageHandler(filters.LOCATION, point_activation)
+    point_activation_handler = MessageHandler(
+        filters.LOCATION, point_activation
+    )
 
     application.add_handler(start_handler, 0)
     application.add_handler(point_activation_handler, 4)
