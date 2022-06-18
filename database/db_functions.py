104699750ac0c4d7e5ec927de5a5f4204880e924
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-18 17:18:04 +0300

    Добавлены функции get_user_callsign, get_users_telegram_id, get_point_time и update_points_in_game_status. Обновлена аннотация типов для некоторых функций.

--- database/db_functions.py
+++ database/db_functions.py
@@ -1,13 +1,24 @@
+from typing import List
+
 from tortoise.exceptions import DoesNotExist
 
 from database.user.models import Team, User, Location
 
 
-async def get_teams() -> list:
+async def get_user_callsign(telegram_id: int) -> str:
+    callsign = await User.get(telegram_id=telegram_id).values('callsign')
+    return callsign['callsign']
+
+
+async def get_users_telegram_id() -> List[int]:
+    telegram_id = await User.all().values('telegram_id')
+    list_of_telegram_id = [tg_id.get('telegram_id') for tg_id in telegram_id]
+    return list_of_telegram_id
+
+
+async def get_teams() -> List[str]:
     teams = await Team.all().values('title')
-
     list_of_teams = [team.get('title') for team in teams]
-
     return list_of_teams
 
 
@@ -15,7 +26,9 @@ async def update_players_team(telegram_id: int,
                               team: str) -> None:
     team_id = await Team.get_or_none(title=team).values('id')
     if await User.filter(telegram_id=telegram_id).exists():
-        await User.filter(telegram_id=telegram_id).update(team_id=team_id['id'])
+        await User.filter(
+            telegram_id=telegram_id
+        ).update(team_id=team_id['id'])
     else:
         raise DoesNotExist
 
@@ -33,11 +46,22 @@ async def get_team_title_by_team_id(team_id: int) -> str:
     return team_title['title']
 
 
-async def get_points() -> list:
+async def get_points() -> List[dict]:
     return await Location.all().values()
 
 
-async def update_points_team_id(point_id: int, team_id: int) -> None:
+async def update_points_team_id(point_id: int,
+                                team_id: int) -> None:
     await Location.filter(
         id=point_id
     ).update(team_id=team_id)
+
+
+async def get_point_time(point_id: int) -> int:
+    time = await Location.get(id=point_id).values('time')
+    return time['time']
+
+
+async def update_points_in_game_status(point_id: int,
+                                       status: bool) -> None:
+    await Location.filter(id=point_id).update(in_game=status)
