ce9037b7c4c00a1240977b5a310b027319eb6431
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-18 17:32:55 +0300

    Удалены CENTER_POINT_LAT и CENTER_POINT_LNG.

--- settings/settings.py
+++ settings/settings.py
@@ -13,10 +13,5 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 CREATORS_ID = os.environ.get('CREATORS_ID')
 
-
-CENTER_POINT_LAT = os.environ.get('CENTER_POINT_LAT')
-CENTER_POINT_LNG = os.environ.get('CENTER_POINT_LNG')
-
-
 if __name__ == '__main__':
     pass
