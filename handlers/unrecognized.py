5b816df7e0ed16b7eec571d5b27b36561e1befa6
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-21 18:29:47 +0300

    Изменен вывод сообщения для пользователя, если он ввел неизвестный текст.

--- handlers/unrecognized.py
+++ handlers/unrecognized.py
@@ -1,3 +1,4 @@
+import telegram.constants
 from telegram import Update
 from telegram.ext import CallbackContext
 
@@ -5,9 +6,11 @@ from telegram.ext import CallbackContext
 async def unrecognized_command(update: Update,
                                context: CallbackContext.DEFAULT_TYPE) -> \
         None:
-    user_input = update.message.text
-    text = f'"{user_input}" - не распознано.'
+    text = f'<b>Для пользователей доступны следующие команды:</b>\n\n' \
+           f'/callsign - регистрация своего позывного в боте\n' \
+           f'/team - выбор игровой стороны'
 
     await update.message.reply_text(
-        text=text
+        text=text,
+        parse_mode=telegram.constants.ParseMode.HTML
     )
