5ed4978967ad4dab9bd82763fad4e035dad42691
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-24 17:47:17 +0300

    Validators added to 'title' of Team, 'callsign' of User, 'point' of Location, 'latitude' of Location, 'longitude' of Location.

--- database/user/models.py
+++ database/user/models.py
@@ -1,10 +1,18 @@
 from tortoise.models import Model
 from tortoise import fields
+from tortoise.validators import MinValueValidator, MaxValueValidator
+
+from database.user.custom_validators import EmptyValueValidator
 
 
 class Team(Model):
     id = fields.IntField(pk=True)
-    title = fields.CharField(max_length=255, unique=True, null=False)
+    title = fields.CharField(
+        max_length=255,
+        unique=True,
+        null=False,
+        validators=[EmptyValueValidator()]
+    )
 
     def __str__(self):
         return self.title
@@ -13,7 +21,12 @@ class Team(Model):
 class User(Model):
     id = fields.IntField(pk=True)
     telegram_id = fields.IntField(unique=True, null=False)
-    callsign = fields.CharField(max_length=255, unique=True, null=True)
+    callsign = fields.CharField(
+        max_length=255,
+        unique=True,
+        null=True,
+        validators=[EmptyValueValidator()]
+    )
     is_admin = fields.BooleanField(default=False)
     in_game = fields.BooleanField(default=False)
     team: fields.ForeignKeyRelation[Team] = fields.ForeignKeyField(
@@ -27,9 +40,22 @@ class User(Model):
 
 class Location(Model):
     id = fields.IntField(pk=True)
-    point = fields.CharField(max_length=255, unique=True, null=False)
-    latitude = fields.FloatField(default=00.000000, null=False)
-    longitude = fields.FloatField(default=00.000000, null=False)
+    point = fields.CharField(
+        max_length=255,
+        unique=True,
+        null=False,
+        validators=[EmptyValueValidator()]
+    )
+    latitude = fields.FloatField(
+        default=00.000000,
+        null=False,
+        validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)]
+    )
+    longitude = fields.FloatField(
+        default=00.000000,
+        null=False,
+        validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)]
+    )
     in_game = fields.BooleanField(default=True, null=False)
     time = fields.FloatField(default=1200.0, null=False)
     team: fields.ForeignKeyRelation[Team] = fields.ForeignKeyField(
