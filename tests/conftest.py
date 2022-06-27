b02e5ac189911435c070cc668547432a9b297245
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-27 10:35:08 +0300

    Removed the autouse setting. The database test run function has been renamed.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ import pytest
 from tortoise.contrib.test import finalizer, initializer
 
 
-@pytest.fixture(scope="session", autouse=True)
-def initialize_tests(request):
+@pytest.fixture(scope="session")
+def initialize_test_db(request):
     db_url = os.environ.get("DATABASE_URL", "sqlite://:memory:")
     initializer(["database.user.models"], db_url=db_url, app_label="models")
     request.addfinalizer(finalizer)
