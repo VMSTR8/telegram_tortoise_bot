359520a9e194294807d1aa1f72dd804dee0dfd88
Maxim Vinokurov <vmstr8@yandex.ru>
2022-06-29 21:29:14 +0300

    Added documentation for validator.

--- database/user/custom_validators.py
+++ database/user/custom_validators.py
@@ -3,6 +3,10 @@ from tortoise.exceptions import ValidationError
 
 
 class EmptyValueValidator(Validator):
+    """
+    Does not allow writing an empty string to the
+    database for CharField, TextField.
+    """
     def __call__(self, value: str):
         if value == '':
-            raise ValidationError(f"Value '{value}' can't be empty!")
+            raise ValidationError("Value can't be empty!")
