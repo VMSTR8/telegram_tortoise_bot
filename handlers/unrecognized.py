7835b4283eed3ebd3146d31c54d21d6a4297935e
Maxim Vinokurov <vmstr8@yandex.ru>
2022-07-01 20:02:11 +0300

    Added a description to unrecognized_command.

--- handlers/unrecognized.py
+++ handlers/unrecognized.py
@@ -3,12 +3,17 @@ from telegram.ext import CallbackContext
 from telegram.constants import ParseMode
 
 
-async def unrecognized_command(update: Update,
-                               context: CallbackContext.DEFAULT_TYPE) -> \
-        None:
-    text = f'<b>Для пользователей доступны следующие команды:</b>\n\n' \
-           f'/callsign - регистрация своего позывного в боте\n' \
-           f'/team - выбор игровой стороны'
+async def unrecognized_command(
+        update: Update,
+        context: CallbackContext.DEFAULT_TYPE
+) -> None:
+    """
+    Sends a message with commands available to users.
+    """
+
+    text = '<b>Для пользователей доступны следующие команды:</b>\n\n' \
+           '/callsign - регистрация своего позывного в боте\n' \
+           '/team - выбор игровой стороны'
 
     await update.message.reply_text(
         text=text,
